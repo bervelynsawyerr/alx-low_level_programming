da5f273c098186ffc4c0580392a8340df2e15aab
Bervelyn Sawyerr-Markwei <bervsawmar@gmail.com>
2022-10-17 12:15:02 -0700

    Print 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 012 to 789
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int i;
+	int g;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+			{
+				if (e != i && e != g && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n')
+		return (0);
+i}
+
