cb7dd1a7b600303da1ebf623fb6776fbd69ecb11
Bervelyn Sawyerr-Markwei <bervsawmar@gmail.com>
2022-10-19 21:36:44 -0700

    Fibonacci 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,9 @@ int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 void print_times_table(int n);
-int main(void)
+int main(void);
+int main(void);
+int main(void);
+int main(void);
 
 #endif
