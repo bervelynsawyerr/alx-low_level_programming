30165c68824de479d30e9a91c61c490042f323c8
Bervelyn Sawyerr-Markwei <bervsawmar@gmail.com>
2022-11-03 15:05:43 -0700

    Strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - a function that returns the length of a string
  * @s: The string
- * Return: lenght
+ * Return: length
  */
 int _strlen_recursion(char *s)
 {
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 	if (*s == 0)
 		return (0);
 
-	length = _strlen_recursio(s + 1);
+	length = _strlen_recursion(s + 1);
 
-	return (lenght + 1);
+	return (length + 1);
 }
